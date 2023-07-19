1083d94316d0147cfe0fb6ad4bf829247d1bbb73
maverick-go <139732686+maverick-go@users.noreply.github.com>
2023-07-19 13:58:12 +0700

    first version of Flutter first app (done by using stateful, stateless widget)

---
+++ first_flutter_app/README.md
@@ -0,0 +1,6 @@
+# first_flutter_app
+
+The first Flutter project in Golden Owl intership.
+
+## Choose device
+- Choose the Macos or Windows device (desktop)
---
+++ first_flutter_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:english_words/english_words.dart';
+import 'package:first_flutter_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First Flutter app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
