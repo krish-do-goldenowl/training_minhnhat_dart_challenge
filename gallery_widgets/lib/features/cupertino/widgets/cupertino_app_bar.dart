2886634c54d9c4ad6656eff3e44dbda73487618d
maverick-go <139732686+maverick-go@users.noreply.github.com>
2023-07-21 17:48:56 +0700

    complete "Cupertino Widget" task

---
+++ gallery_widgets/lib/features/cupertino/widgets/cupertino_app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+class CupertinoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CupertinoAppBar({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(100.0),
+      child: Container(
+        alignment: Alignment.bottomLeft,
+        height: 87,
+        child: CupertinoNavigationBar(
+          border: const Border(
+            bottom: BorderSide(
+              color: Color.fromARGB(77, 154, 154, 154),
+              width: 0.0, // 0.0 means one physical pixel
+            ),
+          ),
+          leading: Transform.scale(
+            scaleY: 1.2,
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100.0);
+}
--- gallery_widgets/lib/router/router.dart
+++ gallery_widgets/lib/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_widgets/features/cupertino/view/cupertino_screen.dart';
 import 'package:gallery_widgets/features/home/view/home_screen.dart';
 import 'package:gallery_widgets/router/route_names.dart';
 
@@ -9,6 +10,12 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (context) {
             return const HomeScreen();
+          },
+        );
+      case RouteNames.cupertino:
+        return MaterialPageRoute(
+          builder: (context) {
+            return const CupertinoScreen();
           },
         );
       default:
