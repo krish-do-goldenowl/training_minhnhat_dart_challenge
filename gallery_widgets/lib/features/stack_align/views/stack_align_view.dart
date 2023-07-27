8c1bc23bc950090bb89efe54ae455bb676518dd8
maverick-go <139732686+maverick-go@users.noreply.github.com>
2023-07-27 16:14:08 +0700

    add BLoC, use emit of Cubit to change state instead of setState

--- gallery_widgets/lib/features/stack_align/views/stack_align_view.dart
+++ gallery_widgets/lib/features/stack_align/views/stack_align_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gallery_widgets/features/stack_align/cubit/stack_align_cubit.dart';
 import 'package:gallery_widgets/features/stack_align/widgets/setting_view.dart';
 
 class StackAlignView extends StatefulWidget {
@@ -46,42 +48,40 @@ class _StackAlignViewState extends State<StackAlignView> {
       appBar: AppBar(
         title: const Text('Stack & Align'),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: SettingView(
-          onSettingChanged: onSettingChanged,
-          alignment: alignment,
-          textDirection: textDirection,
-          stackFit: stackFit,
-          clip: clip,
-        ),
+      bottomNavigationBar: const BottomAppBar(
+        child: SettingView(),
       ),
       body: Center(
-        child: Stack(
-          alignment: alignment,
-          fit: stackFit,
-          clipBehavior: clip,
-          textDirection: textDirection,
-          children: [
-            Container(
-              width: 250,
-              height: 250,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 190,
-              height: 190,
-              color: Colors.greenAccent[400],
-            ),
-            Positioned(
-              right: 0,
-              top: 200,
-              child: Container(
-                width: 120,
-                height: 120,
-                color: Colors.orange,
-              ),
-            ),
-          ],
+        child: BlocBuilder<StackAlignCubit, StackAlignState>(
+          builder: (context, state) {
+            return Stack(
+              alignment: state.alignment,
+              fit: state.stackFit,
+              clipBehavior: state.clip,
+              textDirection: state.textDirection,
+              children: [
+                Container(
+                  width: 250,
+                  height: 250,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 190,
+                  height: 190,
+                  color: Colors.greenAccent[400],
+                ),
+                Positioned(
+                  right: 0,
+                  top: 200,
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    color: Colors.orange,
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
